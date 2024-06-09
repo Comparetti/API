5508f606963ec2df5a25c9ce2c402baa3e90da44
Fernando <fernando.comparetti.hrz@outlook.com>
2024-06-09 13:39:02 -0300

    Adicionar arquivos de projeto.

---
+++ Thunders/Controllers/ProdutorController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Thunders.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProdutorController : ControllerBase
+    {
+    }
+}
