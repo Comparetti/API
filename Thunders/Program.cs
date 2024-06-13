56515602cd2e3f2414ddba9b461eb7e5a57183c7
Fernando <fernando.comparetti.hrz@outlook.com>
2024-06-12 22:22:18 -0300

    Configuracao Docker

--- Thunders/Program.cs
+++ Thunders/Program.cs
@@ -13,28 +13,12 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddScoped<IProdutorService, ProdutorService>();
 builder.Services.AddScoped<IConsumidorService, ConsumidorService>();
 
-if (builder.Configuration.GetValue<bool>("FeatureToggles:UseInMemoryDatabase"))
-{
-    builder.Services.AddDbContext<AppDbContext>(options =>
-        options.UseInMemoryDatabase("DatabaseName"));
-}
-else
-{
-    builder.Services.AddDbContext<AppDbContext>(options =>
-        options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-}
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
-// Configuração para migrações, se não estiver usando banco de dados em memória
-if (!builder.Configuration.GetValue<bool>("FeatureToggles:UseInMemoryDatabase"))
-{
-    using (var scope = app.Services.CreateScope())
-    {
-        var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-        dbContext.Database.Migrate();
-    }
-}
 
 if (app.Environment.IsDevelopment())
 {
